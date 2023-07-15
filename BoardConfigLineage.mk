LineageOS/android_device_google_gs201

Allow overriding kernel defconfig
Change-Id: I6bd52f57c976ac77c0b42b88162290564f2bd00e

BoardConfigLineage.mk
@@ -8,7 +8,7 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
-TARGET_KERNEL_CONFIG := cloudripper_gki_defconfig
+TARGET_KERNEL_CONFIG ?= cloudripper_gki_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/gs201/private/gs-google
 TARGET_NEEDS_DTBOIMAGE := true
 

ShelbyHell/gs101

BoardConfigLineage.mk
@@ -8,7 +8,7 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
-TARGET_KERNEL_CONFIG := slider_gki_defconfig
+TARGET_KERNEL_CONFIG ?= slider_gki_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/gs201/private/gs-google
 TARGET_NEEDS_DTBOIMAGE := true
 